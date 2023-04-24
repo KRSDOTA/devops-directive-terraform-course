898c237c36918b46c211ea35debec2df56c6e80e
Rupert Hammond <meepo0nly2@gmail.com>
2023-04-24 07:12:35 +0100

    Tweaking resources

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -21,23 +21,23 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+resource "rupert_tfstate" "terraform_state" {
   bucket        = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = rupert_tfstate.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket 
+  bucket        = rupert_tfstate.terraform_state.bucket 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
